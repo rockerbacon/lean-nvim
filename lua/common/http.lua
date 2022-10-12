094984aa7cf3a90ea1de5e66384dd458b61c03e5
rockerbacon <vitor.santos@lab309.com.br>
2022-10-12 17:34:31 -0300

    Separate download functionality in small functions and document functions

--- lua/common/http.lua
+++ lua/common/http.lua
@@ -1,9 +1,19 @@
 local fs = require('common.filesystem')
 local path = require('common.path')
 
+--- Gets the path to a downloaded resource
+--- @param url string HTTP URL for the resource
+--- @return string Path where the downloaded resource resides
+local function get_download_destination(url)
+	return path.downloads..'/'..fs.get_basename(url)
+end
+
+--- Downloads a resource to a standard directory
+--- Download location can be acquired using get_download_destination()
+--- @param url string HTTP URL for the resource
+--- @return string Path to the downloaded resource
 local function download(url)
-	local filename = fs.get_basename(url)
-	local filepath = path.downloads..'/'..filename
+	local filepath = get_download_destination(url)
 
 	local success = os.execute("curl -L -XGET '"..url.."' -o '"..filepath.."' &> /dev/null")
 	if not success then
@@ -15,5 +25,6 @@ end
 
 return {
 	download = download,
+	get_download_destination = get_download_destination
 }
 
