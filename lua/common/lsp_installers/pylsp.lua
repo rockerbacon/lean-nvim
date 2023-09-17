049fe1329f66fc8bea7ad6b891ca0b035e1c43b6
Vitor Santos <vitor.santos@loka.com>
2023-09-17 17:09:56 -0300

    add pylsp support

---
+++ lua/common/lsp_installers/pylsp.lua
@@ -0,0 +1,23 @@
+local Builder = require('common.lsp_installers.builder')
+local LspInstallerAction = require('common.lsp_installers.builder.action')
+
+local check_command_unavailable = require('common.lsp_installers.builder.check_command_unavailable')
+
+local server_name = 'pylsp'
+
+local installer = Builder.start(server_name)
+	:check_install_requirement(check_command_unavailable('pylsp'))
+	:exec(LspInstallerAction.new(
+		function()
+			error("pylsp not available, please install through your distribution's package manager")
+		end,
+		function() end,
+		nil
+	))
+:finish()
+
+return {
+	install = installer,
+	settings = nil,
+}
+
